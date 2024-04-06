a5a61b34b6deb700c1830c202f326165f865b117
Profex993 <68472681+Profex993@users.noreply.github.com>
2024-04-06 18:03:32 +0200

    MouseHandler fix

--- src/client/clientMain/MouseHandler.java
+++ src/client/clientMain/MouseHandler.java
@@ -5,6 +5,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class MouseHandler implements MouseListener {
+    public boolean leftClick = false;
 
     @Override
     public void mouseClicked(MouseEvent e) {
@@ -12,10 +13,16 @@ public class MouseHandler implements MouseListener {
 
     @Override
     public void mousePressed(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            leftClick = true;
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            leftClick = false;
+        }
     }
 
     @Override
